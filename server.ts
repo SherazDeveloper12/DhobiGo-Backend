f12ee6f10f3318e20ac0ccdb40451dcadfa0b228
SherazDeveloper12 <talhayounus065@gmail.com>
2025-11-27 23:30:52 +0500

    feat: add body-parser middleware and enhance error handling in server

--- server.ts
+++ server.ts
@@ -1,9 +1,29 @@
+import { stat } from "fs"
+
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 const port = 3000
 
+app.use(express.json())
+app.use(bodyParser.urlencoded())
+app.use(bodyParser.json())
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  try {
+    const data = { sample: 'data' }
+    res.status(200).json({ 
+      status: 'success',
+      data: data,    
+      message: 'Hello World!' }) 
+  } catch (error) {
+    res.status(500).json({ 
+      status: 'error',
+      message: 'Internal Server Error' ,
+      error: error
+    })
+    
+  }
 })
 
 app.listen(port, () => {
